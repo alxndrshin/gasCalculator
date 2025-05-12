42422eb123d5a06710839c2af66cf43edfa7b8f0
alxndrshin <alxndrshin@gmail.com>
2025-05-12 04:05:14 -0400

    changed fileRead and fileWrite to semi private methods (it irks me python doesnt support fully private methods but i digress) also added give and get data these methods are how the class should be interacted with

--- fileInOut.py
+++ fileInOut.py
@@ -2,23 +2,30 @@ import csv
 
 class fileInOut:
 
-    #def __init__():
-      
+    
+
+    def __init__(self):
+        self.dataList=[]
+
     #opens and reads in file
     #file in format date-miles-price 
     #date in format yyyy-mm-dd miles to tenths prices to hundredths
     #return is a list of lists 
-    def fileRead():
-        dataList=[]
+    def __fileRead(self):
         with open('gasData.csv',newline='' ) as fileData:
             fileReader= csv.reader(fileData)
             for row in fileReader:
-                dataList.append(row)
-        print(dataList)
-        return dataList
+                self.dataList.append(row) 
 
     #writes to the file
-    def fileWrite(data):
+    def __fileWrite(self,data):
         with open('gasData.csv','a',newline='\n') as fileData:
             fileWriter=csv.writer(fileData)
             fileWriter.writerow(data)
+    
+    def giveData(self,inputData):
+        self.__fileWrite(inputData)
+
+    def getData(self):
+        self.__fileRead()
+        return self.dataList
