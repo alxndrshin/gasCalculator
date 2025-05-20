0c0079e76013853447113f75e251614ccb3b12e1
alxndrshin <alxndrshin@gmail.com>
2025-05-20 03:27:14 -0400

    copied code directly from https://www.geeksforgeeks.org/tkinter-application-to-switch-between-different-page-frames/ going to change variable names and button functionality to work with the intention of the program and see if it works as intended

--- gui.py
+++ gui.py
@@ -47,79 +47,129 @@ import datetime
 #    root.mainloop()
 
 
-class gui(tk.Tk):
-   def __init__(self,*args,**kwargs):
-      tk.Tk.__init__(self, *args, **kwargs)
-      
-      container=tk.Frame(self)
-      container.pack(side = "top", fill = "both", expand = True)
-      container.grid_rowconfigure(0, weight = 1)
-      container.grid_columnconfigure(0, weight = 1)
-      
-      self.frames={}
-
-      for F in (welcomePage,dataPage,calcPage):
-         frame=F(container,self)
-         self.frames[F]=frame
-
-         frame.grid(row = 0, column = 0, sticky ="nsew")
-
-      self.showFrame=(welcomePage)
-   
-   def showFrame(self, pageName):
-      frame = self.frames[pageName]
-      frame.tkraise()
-
-  
-
-
-class welcomePage(tk.Frame):
-   def __init__(self,parent,controller):
-      tk.Frame.__init__(self, parent)
+LARGEFONT =("Verdana", 35)
+ 
+class tkinterApp(tk.Tk):
+    
+    # __init__ function for class tkinterApp 
+    def __init__(self, *args, **kwargs): 
         
-      label = ttk.Label(self, text ="Welcome Page")
-      label.grid(row = 0, column = 4, padx = 10, pady = 10) 
-
-      dataButton=ttk.Button(self, text ="Data Input",command = lambda : controller.showFrame(dataPage))
-      dataButton.grid(row = 1, column = 1, padx = 10, pady = 10)
-
-      calcButton = ttk.Button(self, text ="Gas Calculator",command = lambda : controller.showFrame(calcPage))
-      calcButton.grid(row = 2, column = 1, padx = 10, pady = 10)
-
-class dataPage(tk.Frame):
-   def __init__(self,parent,controller):
-      tk.Frame.__init__(self, parent)
-      label = ttk.Label(self, text ="Data Input")
-      label.grid(row = 0, column = 4, padx = 10, pady = 10)
-
-      backButton = ttk.Button(self, text ="Back",command = lambda : controller.showFrame(welcomePage))
-      backButton.grid(row = 1, column = 4, padx = 10, pady = 10)
-
-      
-   
-   
-
-   def input(self):
-      fileHandler=fileInOut()
-      try:
-         data=[]
-         newMiles=self.milesInput.get()
-         newCost=self.costInput.get()
-         newMileage=self.mileageInput.get()
-         data=[datetime.date.today(),newMiles,newCost,newMileage]
-         fileHandler.giveData(data)
-      except ValueError:
-         pass
-
-
-class calcPage(tk.Frame):
-   def __init__(self, parent, controller):
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+        
+        # creating a container
+        container = tk.Frame(self)  
+        container.pack(side = "top", fill = "both", expand = True) 
+ 
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+ 
+        # initializing frames to an empty array
+        self.frames = {}  
+ 
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (StartPage, Page1, Page2):
+ 
+            frame = F(container, self)
+ 
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with 
+            # for loop
+            self.frames[F] = frame 
+ 
+            frame.grid(row = 0, column = 0, sticky ="nsew")
+ 
+        self.show_frame(StartPage)
+ 
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+ 
+# first window frame startpage
+ 
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller): 
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text ="Gas Calculator")
+        
+        # label of frame Layout 2
+        label = ttk.Label(self, text ="Startpage", font = LARGEFONT)
+        
+        # putting the grid in its place by using
+        # grid
+        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+ 
+        button1 = ttk.Button(self, text ="Page 1",
+        command = lambda : controller.show_frame(Page1))
+    
+        # putting the button in its place by
+        # using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+ 
+        ## button to show frame 2 with text layout2
+        button2 = ttk.Button(self, text ="Page 2",
+        command = lambda : controller.show_frame(Page2))
+    
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+ 
+         
+ 
+ 
+# second window frame page1 
+class Page1(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Page 1", font = LARGEFONT)
         label.grid(row = 0, column = 4, padx = 10, pady = 10)
-
-        backButton = ttk.Button(self, text ="Back",command = lambda : controller.showFrame(welcomePage))
-        backButton.grid(row = 1, column = 4, padx = 10, pady = 10)
-
-guiTester=gui()
-guiTester.mainloop()
+ 
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="StartPage",
+                            command = lambda : controller.show_frame(StartPage))
+    
+        # putting the button in its place 
+        # by using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+ 
+        # button to show frame 2 with text
+        # layout2
+        button2 = ttk.Button(self, text ="Page 2",
+                            command = lambda : controller.show_frame(Page2))
+    
+        # putting the button in its place by 
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
+ 
+ 
+ 
+ 
+# third window frame page2
+class Page2(tk.Frame): 
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text ="Page 2", font = LARGEFONT)
+        label.grid(row = 0, column = 4, padx = 10, pady = 10)
+ 
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(self, text ="Page 1",
+                            command = lambda : controller.show_frame(Page1))
+    
+        # putting the button in its place by 
+        # using grid
+        button1.grid(row = 1, column = 1, padx = 10, pady = 10)
+ 
+        # button to show frame 3 with text
+        # layout3
+        button2 = ttk.Button(self, text ="Startpage",
+                            command = lambda : controller.show_frame(StartPage))
+    
+        # putting the button in its place by
+        # using grid
+        button2.grid(row = 2, column = 1, padx = 10, pady = 10)
