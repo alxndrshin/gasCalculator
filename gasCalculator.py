b7ffe46680468620e9fc9daf4b5db24b8ec4a3f7
alxndrshin <alxndrshin@gmail.com>
2025-05-23 04:54:37 -0400

    changes to gas calculator some reason getting divide by zero error in calc totaGallons

--- gasCalculator.py
+++ gasCalculator.py
@@ -1,16 +1,24 @@
 import datetime
+import fileInOut
 
 class gasCalculator:
      #data in format date-miles-price or miles-price
     def __init__(self):
+        self.dataLoader=fileInOut.fileInOut()
         self.data=[]
         self.workingData=[]
+        self.mpd=0
+        self.mpg=0
 
     #instance variables: rawData, workingData
     def __init__(self,data):
+        self.mpd=0
+        self.mpg=0
+        self.dataLoader=fileInOut.fileInOut()
         self.rawData=data
         self.workingData=[]
         self.__calcWorkingData(self.rawData)
+        self.calculate()
         #print(self.workingData)
 
  #efficiency will be calculated using the cost 1 previous to the milage used
@@ -26,24 +34,42 @@ class gasCalculator:
             newData.append(each[3])
             self.workingData.append(newData)
 
-    def __calcTotalEfficiency(self):
-        miles=0
-        price=0
-        efficiency=0
-        #print(self.workingData)
-        for row in self.workingData:
-            miles+=int(row[0])
-            price+=int(row[1])
-        efficiency= float(miles/price)
-        return efficiency
+    def calculate(self):
+        #calculates different efficiencies to be stored for retrieval
+        #mpd mpg
+        # miles-price-gallons
+        datapoints=0
+        totalMiles=0
+        totalGallons=0
+        totalDollars=0
+
+        for item in self.workingData:
+            totalMiles+=item[0]
+            totalGallons+=item[1]
+            totalDollars+=item[2]
+            datapoints+=1
+
+        self.mpg=totalMiles/totalGallons
+        self.mpd=totalMiles/totalDollars
+
+    def updateData(self):
+        self.rawData=self.dataLoader.getData()
+        self.__calcWorkingData(self.rawData)
+        
 
     def updateData(self, newData):
         self.rawData=newData
         self.__calcWorkingData(newData)
 
-    def getEfficiency(self):
-        return(self.__calcTotalEfficiency())    
+    def getMPG(self):
+        return self.mpg
+
+    def getMPD(self):
+        return self.mpd    
     
     def runner(self,choice):
+        self.calculate()
         if choice==1:
-            return self.getEfficiency()
+            return self.getMPG()
+        if choice==2:
+            return self.getMPD()
--- main.py
+++ main.py
@@ -15,31 +15,31 @@ class main():
        
 
 
-    # def driver(self):
-    #     choice=0
-    #     keepGoing= True
-    #     while keepGoing:
-    #         print('What would you like to do today?')
-    #         choice=int(input('0 for quit, 1 for add data, 2 for calculate efficiency, '))
-    #         if choice == 0:
-    #             keepGoing= False
-    #         elif choice == 1:
-    #             choice=int(input('how many data points would you like to add? '))
-    #             for x in range(choice):
-    #                 newData=self.userInput.userInputData()
-    #                 #update CSV file with new data
-    #                 self.files.giveData(newData)
-    #                 #retrieve new data and store
-    #                 self.data=self.files.getData()
-    #         elif choice == 2:
-    #             print('what efficiency would you like to calculate?')
-    #             choice=int(input('1 for total, '))
-    #             #run with newest data
-    #             self.calculator.updateData(self.data)
-    #             print('your efficiency is: ',round(self.calculator.runner(choice),2),' miles/dollar')
-    #         else:
-    #             print(choice,' That is not an available option')
-    #     print('thank you') cntrl+/
+    def driver(self):
+        choice=1000000000000
+        keepGoing= True
+        while keepGoing:
+            print('What would you like to do today?')
+            choice=int(input('0 for quit, 1 for add data, 2 for calculate efficiency, '))
+            if choice == 0:
+                keepGoing= False
+            elif choice == 1:
+                choice=int(input('how many data points would you like to add? '))
+                for x in range(choice):
+                    newData=self.userInput.userInputData()
+                    #update CSV file with new data
+                    self.files.giveData(newData)
+                    #retrieve new data and store
+                    self.data=self.files.getData()
+            elif choice == 2:
+                print('what efficiency would you like to calculate?')
+                choice=int(input('1 for MPG, 2 for MPD'))
+                #run with newest data
+                self.calculator.updateData(self.data)
+                print('your efficiency is: ',round(self.calculator.runner(choice),2),' miles/dollar')
+            else:
+                print(choice,' That is not an available option')
+        print('Thank you') 
 
 mainCode=main()
 mainCode.driver()
